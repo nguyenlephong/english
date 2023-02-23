1d17578e3e807f00394dfc0b2ed197ea24ff4fb7
Nguyen Le Phong <nguyenlephong1997@gmail.com>
2023-02-23 17:34:33 +0700

    feat: Update mapping data prop component

--- components/card/WordCard.tsx
+++ components/card/WordCard.tsx
@@ -2,30 +2,39 @@ import React from 'react';
 import {Badge, Image} from "antd";
 import "./WordCard.style.scss";
 
-type WordCardPropType = {}
+type WordCardPropType = {
+  word: string;
+  meaning: string;
+  description: string;
+  meme: string;
+  phrase: string;
+  sentences?: string[];
+}
 const WordCard = (props: WordCardPropType) => {
+  const {word, meme, meaning, description, phrase, sentences} = props;
   return (
       <Badge.Ribbon text="Verb" color="green">
         <div className={"word-card"}>
           <div className={"block horizontal"}>
-            <div className={"memo-photo"}>
+            <div className={"meme-photo"}>
               <Image
                 width={"100%"}
                 height={"160px"}
-                src="https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp"
+                src={meme}
               />
             </div>
             <div className={"main-content"}>
-              <div className="word">BOOK</div>
-              <div className="meaning">Đặt chổ</div>
-              <div className="description">to arrange to have something at a certain time</div>
+              <div className="word">{word}</div>
+              <div className="meaning">{meaning}</div>
+              <div className="description">{description}</div>
             </div>
-    
+
           </div>
           <div className="footer green">
-            <div className={"phrase"}><strong>*</strong> Book a table = đặt bàn</div>
-            <div>He book a table at a sushi restaurant</div>
-            <div>Anh ấy đặt bàn tại một nhà hàng sushi</div>
+            <div className={"phrase"}><strong>*</strong> {phrase}</div>
+            {sentences?.map(sentence => {
+              return <div key={sentence} title={sentence}>{sentence}</div>
+            })}
           </div>
         </div>
       </Badge.Ribbon>
--- pages/home.tsx
+++ pages/home.tsx
@@ -7,10 +7,31 @@ export default function Home() {
     <div className="page">
       <Row gutter={[12, 12]}>
         <Col xs={24} md={12}>
-          <WordCard/>
+          <WordCard
+            word={"BOOK"}
+            meaning={"Đặt chổ"}
+            description={"to arrange to have something at a certain time"}
+            meme={"https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp"}
+            phrase={"Book a table = đặt bàn"}
+            sentences={[
+              "He book a table at a sushi restaurant",
+              "Anh ấy đặt bàn tại một nhà hàng sushi",
+            ]}
+          />
         </Col>
+
         <Col xs={24} md={12}>
-          <WordCard/>
+          <WordCard
+            word={"BOOK"}
+            meaning={"Sách"}
+            description={"to arrange to have something at a certain time"}
+            phrase={"Buy a book = mua một quyển sách"}
+            meme={"https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp"}
+            sentences={[
+              "He book a table at a sushi restaurant",
+              "Anh ấy đặt bàn tại một nhà hàng sushi",
+            ]}
+          />
         </Col>
       </Row>
     </div>
