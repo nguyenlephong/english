6262f9752490be0edde0c1eb5254786cb8203f4e
Nguyen Le Phong <nguyenlephong1997@gmail.com>
2023-02-25 06:31:09 +0700

    add: create new a TabBar component

--- src/app/words/page.tsx
+++ src/app/words/page.tsx
@@ -2,39 +2,96 @@
 import React from 'react';
 import WordCard from "@/components/card/WordCard";
 import {Col, Row} from "antd";
+import TabBar from "@/components/tab-bar/TabBar";
 
 export default function Page() {
   return (
     <div className="page">
-      <Row gutter={[12, 12]}>
-        <Col xs={24} md={12}>
-          <WordCard
-            word={"BOOK"}
-            meaning={"Đặt chổ"}
-            description={"to arrange to have something at a certain time"}
-            meme={"https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp"}
-            phrase={"Book a table = đặt bàn"}
-            sentences={[
-              "He book a table at a sushi restaurant",
-              "Anh ấy đặt bàn tại một nhà hàng sushi",
-            ]}
-          />
-        </Col>
-        
-        <Col xs={24} md={12}>
-          <WordCard
-            word={"BOOK"}
-            meaning={"Sách"}
-            description={"to arrange to have something at a certain time"}
-            phrase={"Buy a book = mua một quyển sách"}
-            meme={"https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp"}
-            sentences={[
-              "He book a table at a sushi restaurant",
-              "Anh ấy đặt bàn tại một nhà hàng sushi",
-            ]}
-          />
-        </Col>
-      </Row>
+      <div className={"page-body"}>
+        <Row gutter={[12, 12]}>
+          <Col xs={24} md={12}>
+            <WordCard
+              word={"BOOK"}
+              meaning={"Đặt chổ"}
+              description={"to arrange to have something at a certain time"}
+              meme={"https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp"}
+              phrase={"Book a table = đặt bàn"}
+              sentences={[
+                "He book a table at a sushi restaurant",
+                "Anh ấy đặt bàn tại một nhà hàng sushi",
+              ]}
+            />
+          </Col>
+    
+          <Col xs={24} md={12}>
+            <WordCard
+              word={"BOOK"}
+              meaning={"Sách"}
+              description={"to arrange to have something at a certain time"}
+              phrase={"Buy a book = mua một quyển sách"}
+              meme={"https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp"}
+              sentences={[
+                "He book a table at a sushi restaurant",
+                "Anh ấy đặt bàn tại một nhà hàng sushi",
+              ]}
+            />
+          </Col>
+          <Col xs={24} md={12}>
+            <WordCard
+              word={"BOOK"}
+              meaning={"Sách"}
+              description={"to arrange to have something at a certain time"}
+              phrase={"Buy a book = mua một quyển sách"}
+              meme={"https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp"}
+              sentences={[
+                "He book a table at a sushi restaurant",
+                "Anh ấy đặt bàn tại một nhà hàng sushi",
+              ]}
+            />
+          </Col>
+          <Col xs={24} md={12}>
+            <WordCard
+              word={"BOOK"}
+              meaning={"Sách"}
+              description={"to arrange to have something at a certain time"}
+              phrase={"Buy a book = mua một quyển sách"}
+              meme={"https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp"}
+              sentences={[
+                "He book a table at a sushi restaurant",
+                "Anh ấy đặt bàn tại một nhà hàng sushi",
+              ]}
+            />
+          </Col>
+          <Col xs={24} md={12}>
+            <WordCard
+              word={"BOOK"}
+              meaning={"Sách"}
+              description={"to arrange to have something at a certain time"}
+              phrase={"Buy a book = mua một quyển sách"}
+              meme={"https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp"}
+              sentences={[
+                "He book a table at a sushi restaurant",
+                "Anh ấy đặt bàn tại một nhà hàng sushi",
+              ]}
+            />
+          </Col>
+          <Col xs={24} md={12}>
+            <WordCard
+              word={"BOOK"}
+              meaning={"Sách"}
+              description={"to arrange to have something at a certain time"}
+              phrase={"Buy a book = mua một quyển sách"}
+              meme={"https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp"}
+              sentences={[
+                "He book a table at a sushi restaurant",
+                "Anh ấy đặt bàn tại một nhà hàng sushi",
+              ]}
+            />
+          </Col>
+        </Row>
+      </div>
+      
+      <TabBar/>
     </div>
   )
 }
--- src/components/tab-bar/TabBar.tsx
+++ src/components/tab-bar/TabBar.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
+'use client';
+import React, {useState} from 'react';
+import { Badge, TabBar } from 'antd-mobile';
+import {Gear, House, File, Student, Notebook} from "phosphor-react"
 type TabBarPropType = {}
-const TabBar = (props: TabBarPropType) => {
+const TabBarCustomization = (props: TabBarPropType) => {
+  const tabs = [
+    {
+      key: 'home',
+      title: 'Home',
+      icon: <House size={32} />,
+      badge: Badge.dot,
+    },
+    {
+      key: 'words',
+      title: 'words',
+      icon: <File size={32} />,
+      badge: '5',
+    },
+    {
+      key: 'study',
+      title: 'Study',
+      icon: (active: boolean) =>
+        active ? <Student size={32} /> : <Notebook size={32} />,
+      badge: '99+',
+    },
+    {
+      key: 'settings',
+      title: 'Settings',
+      icon: <Gear size={32} />,
+    },
+  ]
+  
+  const [activeKey, setActiveKey] = useState('todo')
   return (
     <div>
-      TabBar
+      <TabBar activeKey={activeKey} onChange={setActiveKey}>
+        {tabs.map(item => (
+          <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+        ))}
+      </TabBar>
     </div>
   );
 };
 
-export default TabBar;
+export default TabBarCustomization;
